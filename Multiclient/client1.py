ed259045c720bd97f5fcb79ebb7ebeae251a406a
Manav Patnaik <manavpatnaik@hotmail.com>
2021-07-29 07:11:01 +0530

    Client socket created

---
+++ Multiclient/client1.py
@@ -0,0 +1,25 @@
+import socket
+
+PORT = 12234
+SERVER = socket.gethostname()
+ADDR = (SERVER, PORT)
+
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+# Create socket object and connect to server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+# Method to send message to server
+def send(msg):
+    message = msg.encode(FORMAT)
+    client.send(message)
+
+
+while True:
+    msg = input('Message to be sent to the server: ')
+    send(msg)
+    if msg == DISCONNECT_MESSAGE:
+        break
+
